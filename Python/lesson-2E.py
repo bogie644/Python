6923dc64bda2322a6ee5e4217d6140705aca2d8e
Bogie644 <bogie@list.ru>
2019-05-06 00:15:59 +0300

    Домашнее задание 3 урок

---
+++ Python/lesson-2E.py
@@ -0,0 +1,26 @@
+#Задание-1
+
+# fruits = ['яблоко','банан','киви','арбуз']
+# print(fruits)
+# i = 0
+# for fruit in fruits:
+#     i += 1
+#     print(i, '{:>10}'.format(fruit))
+
+
+#Задание-2
+
+# my_list1 = ['qwe','rty','asd','fgh','zxc','cvb']
+# my_list2 = ['ytr','ewq','asd','fgh','ghj']
+# print(list(set(my_list1)-set(my_list2)))
+
+#Задание-3
+
+# number_list = [1,2,3,4,44,55,66,12,23,34,65,94]
+# result_list = []
+# for number in number_list:
+#     if int(number)%2:
+#         result_list.append(number*2)
+#     else:result_list.append(number/4)
+# print(number_list)
+# print(result_list)
